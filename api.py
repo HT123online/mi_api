6dd15789806f54a4f88ee562d74aa6532f6c7970
Sebastian-Espiritu <95195419+Sebastian-Espiritu@users.noreply.github.com>
2026-08-29 23:51:04 -0500

    Cree almacenamiento JSON y probe unas funciones

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from flask import Flask, send_from_directory,jsonify,request,send_file
 import qrcode
+import json #cambio n1
+import uuid
 from io import BytesIO
 
 app=Flask(__name__)
@@ -26,5 +28,81 @@ def pross(monto,ref):
     return jsonify(data)
 
 
+def guardarQr(nuevo_qr): #cambio n2
+    with open("qr_data.json", "r") as archivo:
+        datos = json.load(archivo)
+ 
+    for qr in datos["qrs"]:
+ 
+        if qr["idQR"] == nuevo_qr["idQR"]:
+            print("QR ya existe")
+            return False
+ 
+    datos["qrs"].append(nuevo_qr)
+ 
+    with open("qr_data.json", "w") as archivo:
+        json.dump(datos, archivo, indent=4)
+ 
+    return True
+
+def buscarQr(idQR): #cambio n3 importante para ver si es el mismoqr q ya enviaron
+
+    with open("qr_data.json", "r") as archivo:
+        datos = json.load(archivo)
+
+    for qr in datos["qrs"]:
+
+        if qr["idQR"] == idQR:
+            return qr
+
+    return None
+
+def marUsado(idQR): #cambio n3
+
+    with open("qr_data.json", "r") as archivo:
+        datos = json.load(archivo)
+
+    for qr in datos["qrs"]:
+
+        if qr["idQR"] == idQR:
+
+            qr["usado"] = True
+
+            with open("qr_data.json", "w") as archivo:
+                json.dump(datos, archivo, indent=4)
+
+            return True
+
+    return False
+qr = buscarQr("QR123")
+
+def verReplayAttack(idQR): #cambio n4
+    qr = buscarQr(idQR)
+
+    if qr is None:
+        return False
+
+    return qr["usado"]
+
+def procesarPago(idQR): #cambio n4
+
+    if verReplayAttack(idQR):
+        return "Replay Attack"
+
+    marUsado(idQR)
+
+    return "Pago Aceptado"
+
+
 if __name__=="__main__":
     app.run(debug=True)
+
+# guardarQr({
+#     "idQR": "QR666",
+#     "monto": 40,
+#     "referencia": "hamburguesa",
+#     "expiracion": "2026-08-30",
+#     "usado": False
+# })
+# print(procesarPago("QR992"))
+# print(procesarPago("QR992"))
